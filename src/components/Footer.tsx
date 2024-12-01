80f78c34b0b55297d7c505a0c83f45b0183eae22
ibrahim <ibrahimomobolaji1999@gmail.com>
2024-12-01 11:45:49 +0100

    style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 const Footer = () => {
   return (
     <div className="px-6 md:px-0 mt-12 text-white/70 py-8 container mx-auto border-t border-gray-700 pt-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Ibrahim Omobolaji</h1>
+      <h1 className="text-2xl font-bold text-orange-400">Ibrahim <span className='hidden md:visible'>Omobolaji Ibrahim</span></h1>
       <div className='flex space-x-6 mt-4'>
         <a href="#" className="hover:text-gray-300">
           <FaLinkedin size={20} />
